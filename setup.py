1a2522ffe23c8a35abfbf738297c3c1eb4b31fae
Sitt Guruvanich <aekasitt@gmail.com>
2020-11-26 23:12:28 +0700

    Added python 3.9 in classifiers

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Operating System :: OS Independent',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development :: Libraries :: Python Modules'
