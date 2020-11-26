4ecd7145d3866d091d56cc26f9f5a95f46646ffb
Sitt Guruvanich <aekasitt@gmail.com>
2020-11-26 17:00:09 +0700

    [Fix] Missing comma

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3.8',
     'Operating System :: OS Independent',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development :: Libraries :: Python Modules'
