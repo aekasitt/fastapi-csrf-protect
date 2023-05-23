11eedba9f7089fca192ad7becad78be3235c7877
Sitt Guruvanich <aekazitt@gmail.com>
2023-05-23 18:37:11 +0700

    Add MyPy type-hints to exception classes

--- fastapi_csrf_protect/exceptions.py
+++ fastapi_csrf_protect/exceptions.py
@@ -9,21 +9,21 @@
 # HISTORY:
 # *************************************************************
 class CsrfProtectError(Exception):
-    def __init__(self, status_code, message):
+    def __init__(self, status_code: int, message: str):
         self.status_code = status_code
         self.message = message
 
 
 class InvalidHeaderError(CsrfProtectError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(422, message)
 
 
 class MissingTokenError(CsrfProtectError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(400, message)
 
 
 class TokenValidationError(CsrfProtectError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super().__init__(401, message)
