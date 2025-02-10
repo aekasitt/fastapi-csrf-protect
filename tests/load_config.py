4013c71175dd055e3a3a241471d16f9000c5dbf6
Sitt Guruvanich <aekazitt@gmail.com>
2025-02-10 23:47:50 +0700

    test: separate invalid and valid load_config tests

--- tests/load_config.py
+++ tests/load_config.py
@@ -14,104 +14,113 @@ from typing import Tuple, Union
 
 ### Third-party packages ###
 from pydantic import ValidationError
-from pytest import mark
+from pytest import mark, raises
 
 ### Local modules ###
 from fastapi_csrf_protect import CsrfProtect
 
 
 @mark.parametrize(
-  "csrf_settings, valid",
+  "csrf_settings",
   (
-    ((("header_name", 2),), False),
-    ((("header_name", 1.0),), False),
-    ((("header_name", True),), False),
-    ((("header_name", b"header_name"),), False),
-    ((("header_name", "header_name"),), True),
-    ((("header_name", []),), False),
-    ((("header_name", {}),), False),
-    ((("header_type", 2),), False),
-    ((("header_type", 1.0),), False),
-    ((("header_type", True),), False),
-    ((("header_type", b"header_type"),), False),
-    ((("header_type", "header_type"),), True),
-    ((("header_type", []),), False),
-    ((("header_type", {}),), False),
-    ((("httponly", False),), True),
-    ((("httponly", None),), True),
-    ((("httponly", True),), True),
-    ((("httponly", "false"),), False),
-    ((("httponly", b"true"),), False),
-    ((("methods", 2),), False),
-    ((("methods", 1.0),), False),
-    ((("methods", True),), False),
-    ((("methods", b"GET, POST"),), False),
-    ((("methods", "GET, POST"),), False),
-    ((("methods", []),), True),
-    ((("methods", {}),), False),
-    ((("methods", [1, 2, 3]),), False),
-    ((("methods", (1, 2, 3)),), False),
-    ((("methods", {1, 2, 3}),), False),
-    ((("methods", ["1", "2", "3"]),), False),
-    ((("methods", ("1", "2", "3")),), False),
-    ((("methods", {"1", "2", "3"}),), False),
-    ((("methods", ["GET", "POST", "DELETE"]),), True),
-    ((("methods", ("GET", "POST", "DELETE")),), True),
-    ((("methods", {"GET", "POST", "DELETE"}),), True),
-    ((("methods", {"key": "value"}),), False),
-    ((("secret_key", 2),), False),
-    ((("secret_key", 1.0),), False),
-    ((("secret_key", True),), False),
-    ((("secret_key", b"secret"),), False),
-    ((("secret_key", "secret"),), True),
-    ((("secret_key", []),), False),
-    ((("secret_key", {}),), False),
-    ((("token_location", "body"),), False),  # missing token_key
-    ((("token_location", "body"), ("token_key", "csrf-token")), True),
-    ((("token_location", b"body"), ("token_key", "csrf-token")), False),
-    ((("token_location", "header"),), True),
-    ((("token_location", b"header"),), False),
-    ((("cookie_samesite", None),), False),
-    ((("cookie_samesite", None), ("cookie_secure", True)), True),
-    ((("cookie_samesite", "lax"),), True),
-    ((("cookie_samesite", "none"),), False),
-    ((("cookie_samesite", "none"), ("cookie_secure", True)), True),
-    ((("cookie_samesite", "strict"),), True),
-    ((("cookie_samesite", b"lax"),), False),
-    ((("cookie_samesite", b"none"),), False),
-    ((("cookie_samesite", b"strict"),), False),
-    ((("cookie_samesite", "null"),), False),
-    ((("cookie_samesite", b"null"),), False),
-    ((("cookie_samesite", 0),), False),
-    ((("cookie_samesite", 1),), False),
-    ((("cookie_samesite", True),), False),
-    ((("cookie_samesite", False),), False),
-    ((("cookie_samesite", 2.0),), False),
-    ((("cookie_samesite", {1, 2, 3}),), False),
-    ((("cookie_samesite", {1.0, 2.0, 3.0}),), False),
-    ((("cookie_samesite", {"1", "2", "3"}),), False),
-    ((("cookie_samesite", [1, 2, 3]),), False),
-    ((("cookie_samesite", [1.0, 2.0, 3.0]),), False),
-    ((("cookie_samesite", ["1", "2", "3"]),), False),
-    ((("cookie_samesite", {"key": "value"}),), False),
-    ((("cookie_secure", False),), True),
-    ((("cookie_secure", None),), True),
-    ((("cookie_secure", True),), True),
-    ((("cookie_secure", "false"),), False),
-    ((("cookie_secure", b"true"),), False),
+    (("cookie_samesite", None),),
+    (("cookie_samesite", "none"),),
+    (("cookie_samesite", b"lax"),),
+    (("cookie_samesite", b"none"),),
+    (("cookie_samesite", b"strict"),),
+    (("cookie_samesite", "null"),),
+    (("cookie_samesite", b"null"),),
+    (("cookie_samesite", 0),),
+    (("cookie_samesite", 1),),
+    (("cookie_samesite", True),),
+    (("cookie_samesite", False),),
+    (("cookie_samesite", 2.0),),
+    (("cookie_samesite", {1, 2, 3}),),
+    (("cookie_samesite", {1.0, 2.0, 3.0}),),
+    (("cookie_samesite", {"1", "2", "3"}),),
+    (("cookie_samesite", [1, 2, 3]),),
+    (("cookie_samesite", [1.0, 2.0, 3.0]),),
+    (("cookie_samesite", ["1", "2", "3"]),),
+    (("cookie_samesite", {"key": "value"}),),
+    (("cookie_secure", "false"),),
+    (("cookie_secure", b"true"),),
+    (("header_name", 2),),
+    (("header_name", 1.0),),
+    (("header_name", True),),
+    (("header_name", b"header_name"),),
+    (("header_name", []),),
+    (("header_name", {}),),
+    (("header_type", 2),),
+    (("header_type", 1.0),),
+    (("header_type", True),),
+    (("header_type", b"header_type"),),
+    (("header_type", []),),
+    (("header_type", {}),),
+    (("httponly", "false"),),
+    (("httponly", b"true"),),
+    (("methods", 2),),
+    (("methods", 1.0),),
+    (("methods", True),),
+    (("methods", b"GET, POST"),),
+    (("methods", "GET, POST"),),
+    (("methods", {}),),
+    (("methods", [1, 2, 3]),),
+    (("methods", (1, 2, 3)),),
+    (("methods", {1, 2, 3}),),
+    (("methods", ["1", "2", "3"]),),
+    (("methods", ("1", "2", "3")),),
+    (("methods", {"1", "2", "3"}),),
+    (("methods", {"key": "value"}),),
+    (("secret_key", 2),),
+    (("secret_key", 1.0),),
+    (("secret_key", True),),
+    (("secret_key", b"secret"),),
+    (("secret_key", []),),
+    (("secret_key", {}),),
+    (("token_location", "body"),),  # missing token_key
+    (("token_location", b"body"), ("token_key", "csrf-token")),
+    (("token_location", b"header"),),
   ),
 )
-def test_load_config(
-  csrf_settings: Tuple[Tuple[str, Union[None, bool, bytes, int, float, str]], ...], valid: bool
-):
-  error_raised: bool = False
-  try:
+def test_load_config_with_invalid_csrf_settings(
+  csrf_settings: Tuple[Tuple[str, Union[None, bool, bytes, float, int, str]], ...],
+) -> None:
+  with raises(ValidationError) as exc_info:
 
     @CsrfProtect.load_config
-    def load_configs() -> Tuple[Tuple[str, Union[None, bool, bytes, int, float, str]], ...]:
+    def _() -> Tuple[Tuple[str, Union[None, bool, bytes, int, float, str]], ...]:
       return csrf_settings
 
-  except Exception as err:
-    error_raised = True
-    assert isinstance(err, ValidationError)
-  assert error_raised is (True, False)[valid]
+  assert exc_info is not None
+
+
+@mark.parametrize(
+  "csrf_settings",
+  (
+    (("header_name", "header_name"),),
+    (("header_type", "header_type"),),
+    (("httponly", False),),
+    (("httponly", None),),
+    (("httponly", True),),
+    (("methods", []),),
+    (("methods", ["GET", "POST", "DELETE"]),),
+    (("methods", ("GET", "POST", "DELETE")),),
+    (("methods", {"GET", "POST", "DELETE"}),),
+    (("secret_key", "secret"),),
+    (("token_location", "body"), ("token_key", "csrf-token")),
+    (("token_location", "header"),),
+    (("cookie_samesite", None), ("cookie_secure", True)),
+    (("cookie_samesite", "lax"),),
+    (("cookie_samesite", "none"), ("cookie_secure", True)),
+    (("cookie_samesite", "strict"),),
+    (("cookie_secure", False),),
+    (("cookie_secure", None),),
+    (("cookie_secure", True),),
+  ),
+)
+def test_valid_load_config(
+  csrf_settings: Tuple[Tuple[str, Union[None, bool, bytes, int, float, str]], ...],
+):
+  @CsrfProtect.load_config
+  def _() -> Tuple[Tuple[str, Union[None, bool, bytes, int, float, str]], ...]:
+    return csrf_settings
