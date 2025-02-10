31df4a8b66d0e61d8ed20b14df8ab5213c600a62
Sitt Guruvanich <aekazitt@gmail.com>
2025-02-11 02:37:29 +0700

    chore: version bump; 1.0.1 -> 1.0.2

--- src/fastapi_csrf_protect/__init__.py
+++ src/fastapi_csrf_protect/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (C) 2020-2025 All rights reserved.
 # FILENAME:    ~~/src/fastapi_csrf_protect/__init__.py
-# VERSION:     1.0.1
+# VERSION:     1.0.2
 # CREATED:     2020-11-25 14:35
 # AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
 # DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
@@ -12,7 +12,7 @@
 FastAPI extension that provides Csrf Protection Token support
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from .core import CsrfProtect
 
