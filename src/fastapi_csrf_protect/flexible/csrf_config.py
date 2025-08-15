b42e49d30438d54a09f1321acf85f1e6ef52962d
Sitt Guruvanich <aekazitt@gmail.com>
2025-08-15 16:43:37 +0700

    chore: trace back to v1.0.3; anti-dry clone

--- src/fastapi_csrf_protect/flexible/csrf_config.py
+++ src/fastapi_csrf_protect/flexible/csrf_config.py
@@ -10,19 +10,34 @@
 # *************************************************************
 
 ### Standard packages ###
-from typing import Any, Callable, Sequence, Tuple, Union
+from typing import Any, ClassVar, Callable, Literal, Optional, Sequence, Set, Tuple, Union, cast
 
 ### Third-party packages ###
 from pydantic import ValidationError
 from pydantic_settings import BaseSettings
 
 ### Local modules ###
-from fastapi_csrf_protect.csrf_config import CsrfConfig as BaseCsrfConfig
-from fastapi_csrf_protect.flexible.load_config import LoadConfig
+from fastapi_csrf_protect.load_config import LoadConfig
 
 
-class CsrfConfig(BaseCsrfConfig):
-  """Same as the base CsrfConfig, except that it uses `flexible.load_config.LoadConfig`."""
+class CsrfConfig(object):
+  _cookie_key: ClassVar[str] = "fastapi-csrf-token"
+  _cookie_path: ClassVar[str] = "/"
+  _cookie_domain: ClassVar[Optional[str]] = None
+  _cookie_samesite: ClassVar[Optional[Literal["lax", "strict", "none"]]] = None
+  _cookie_secure: ClassVar[bool] = False
+  _header_name: ClassVar[str] = "X-CSRF-Token"
+  _header_type: ClassVar[Optional[str]] = None
+  _httponly: ClassVar[bool] = True
+  _max_age: ClassVar[int] = 3600
+  _methods: ClassVar[Set[Literal["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]]] = {
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+  }
+  _secret_key: ClassVar[Optional[str]] = None
+  _token_key: ClassVar[str] = "csrf-token"
 
   @classmethod
   def load_config(
@@ -30,7 +45,19 @@ class CsrfConfig(BaseCsrfConfig):
   ) -> None:
     try:
       config = LoadConfig(**{key.lower(): value for key, value in settings()})
-      cls._load_config(config)
+      cls._cookie_key = config.cookie_key or cls._cookie_key
+      cls._cookie_path = config.cookie_path or cls._cookie_path
+      cls._cookie_domain = config.cookie_domain
+      if config.cookie_samesite in {"lax", "none", "strict"}:
+        cls._cookie_samesite = cast(Literal["lax", "none", "strict"], config.cookie_samesite)
+      cls._cookie_secure = False if config.cookie_secure is None else config.cookie_secure
+      cls._header_name = config.header_name or cls._header_name
+      cls._header_type = config.header_type
+      cls._httponly = True if config.httponly is None else config.httponly
+      cls._max_age = config.max_age or cls._max_age
+      cls._methods = config.methods or cls._methods
+      cls._secret_key = config.secret_key
+      cls._token_key = config.token_key or cls._token_key
     except ValidationError:
       raise
     except Exception as err:
