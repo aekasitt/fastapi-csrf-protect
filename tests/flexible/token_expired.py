31147a466d24f6fed314c4ca5d4ec2061d697415
Eliam LOTONGA <e.m.lotonga@gmail.com>
2025-08-18 08:53:00 +0200

    test: add more comprehensive tests

---
+++ tests/flexible/token_expired.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Copyright (C) 2020-2025 All rights reserved.
+# FILENAME:    ~~/tests/token_expired.py
+# VERSION:     1.0.2
+# CREATED:     2020-11-26 16:14
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from time import sleep
+from typing import Dict, List, Tuple, Union
+
+### Third-party packages ###
+from fastapi.testclient import TestClient
+
+### Local modules ###
+from . import flexible_client
+from fastapi_csrf_protect.flexible import CsrfProtect
+
+
+def test_validate_token_expired(flexible_client: TestClient, max_age: int = 2) -> None:
+  ### Load config ###
+  @CsrfProtect.load_config
+  def _() -> List[Tuple[str, Union[int, str]]]:
+    return [("secret_key", "secret"), ("max_age", max_age)]
+
+  ### Generate token ###
+  response = flexible_client.get("/gen-token")
+
+  ### Assertion ###
+  assert response.status_code == 200
+
+  ### Extract `csrf_token` from response to be set as next request's header ###
+  csrf_token: str = response.json().get("csrf_token", None)
+  headers: Dict[str, str] = {"X-CSRF-Token": csrf_token} if csrf_token is not None else {}
+
+  ### Delay until expiry ###
+  sleep(max_age + 1)
+
+  ### Post to protected endpoint ###
+  response = flexible_client.post("/protected", headers=headers)
+
+  ### Assertions ###
+  assert response.status_code == 400
+  assert response.json() == {"detail": "Missing Cookie: `fastapi-csrf-token`."}
---
+++ tests/flexible/token_reuse.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright (C) 2020-2025 All rights reserved.
+# FILENAME:    ~~/tests/token_reuse.py
+# VERSION:     1.0.2
+# CREATED:     2023-06-18 15:07
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from typing import Dict, List, Tuple, Union
+
+### Third-party packages ###
+from fastapi.testclient import TestClient
+from httpx import Response
+
+### Local modules ###
+from . import flexible_client
+from fastapi_csrf_protect.flexible import CsrfProtect
+
+
+def test_disallow_token_reuse(flexible_client: TestClient, max_age: int = 2) -> None:
+  ### Loads config ###
+  @CsrfProtect.load_config
+  def _() -> List[Tuple[str, Union[int, str]]]:
+    return [("secret_key", "secret"), ("max_age", max_age)]
+
+  ### Generate token ###
+  response: Response = flexible_client.get("/gen-token")
+
+  ### Assertion ###
+  assert response.status_code == 200
+
+  ### Extract `csrf_token` from response to be set as next request's header ###
+  csrf_token: str = response.json().get("csrf_token", None)
+  headers: Dict[str, str] = {"X-CSRF-Token": csrf_token} if csrf_token is not None else {}
+
+  ### Post to protected endpoint ###
+  response = flexible_client.post("/protected", headers=headers)
+
+  ### Assertions ###
+  assert response.status_code == 200
+  assert response.json() == {"detail": "OK"}
+
+  ### Immediately get protected contents again ###
+  response = flexible_client.post("/protected", headers=headers)
+
+  ### Assertions ###
+  assert response.status_code == 400
+  assert response.json() == {"detail": "Missing Cookie: `fastapi-csrf-token`."}
