b03abcf450db53e23fe8fdfc892d1d97d232854f
Sitt Guruvanich <aekazitt@gmail.com>
2025-08-19 21:31:50 +0700

    refactor(fmt): format by ruff; add whitespace

--- src/fastapi_csrf_protect/exceptions.py
+++ src/fastapi_csrf_protect/exceptions.py
@@ -9,6 +9,7 @@
 # HISTORY:
 # *************************************************************
 
+
 class CsrfProtectError(Exception):
   def __init__(self, status_code: int, message: str):
     self.status_code = status_code
